eb5a15c8306ae45ad6ddc226afa75b00bcf2389e
seenicroot <sngmanhim@hotmail.com>
2024-05-13 14:37:34 -0400

    Add placeholder music on game start

--- main.gd
+++ main.gd
@@ -2,6 +2,7 @@ extends Node
 
 @export var world_scene: PackedScene
 @export var player_scene: PackedScene
+@export var music: AudioStream
 
 var game_running: bool = false
 var world: Node2D
@@ -12,6 +13,19 @@ var player: CharacterBody2D
 func _ready() -> void:
 	for child in get_children():
 		queue_free()
+	start_game()
+	var music_player = AudioStreamPlayer.new()
+	music_player.autoplay = true
+	music_player.stream = music
+	music_player.volume_db = -20
+	add_child(music_player)
+
+func _process(_delta: float) -> void:
+	if game_running:
+		world_camera.global_position = player.global_position
+
+
+func start_game() -> void:
 	world = world_scene.instantiate()
 	world_camera = world.get_node("Camera2D") as Camera2D
 	add_child(world)
@@ -19,11 +33,6 @@ func _ready() -> void:
 	world.add_child(player)
 	player.item_rect_changed.connect(_on_player_rect_changed)
 	game_running = true
-
-
-func _process(_delta: float) -> void:
-	if game_running:
-		world_camera.global_position = player.global_position
 	
 
 func _on_player_rect_changed() -> void:

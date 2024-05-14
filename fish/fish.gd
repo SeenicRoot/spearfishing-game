c94f16795bc0fef7105720f268100d481ce1b4e0
seenicroot <sngmanhim@hotmail.com>
2024-05-14 14:37:50 -0400

    Allow fish to stay still

--- fish/fish.gd
+++ fish/fish.gd
@@ -1,45 +1,42 @@
 class_name Fish
 extends CharacterBody2D
 
+const VELOCITY_LIMIT: float = 50.0
+
 @export var speed: float = 60
 
 var direction: Vector2
 var previous_direction: Vector2
 
-var is_moving: bool = false
+var can_move: bool = true
 var is_being_attacked: bool = false
-var is_hooked: bool = false
 
-const velocity_limit: float = 50.0
 
 @onready var animated_sprite_2d := $AnimatedSprite2D as AnimatedSprite2D
 @onready var timer := $Timer as Timer
 
 func move(delta: float) -> void:
-	if is_hooked:
-		## Unable to act, dragged towards the player
-		## if harpoon collides fish hitbox, capture
-		is_hooked = false
-	elif is_being_attacked:
+	if is_being_attacked:
 		speed = 120
 		change_sprite_direction()
 		velocity += direction * speed * delta
-		velocity = velocity.limit_length(velocity_limit)
-	elif !is_moving:
+		velocity = velocity.limit_length(VELOCITY_LIMIT)
+	else:
 		speed = 60
 		change_sprite_direction()
 		velocity += direction * speed * delta
-		velocity = velocity.limit_length(velocity_limit/2)
+		velocity = velocity.limit_length(VELOCITY_LIMIT/2)
 	move_and_slide()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	move(delta)
+	if can_move:
+		move(delta)
 
 func _on_timer_timeout() -> void:
 	timer.wait_time = [1.0, 1.5, 2.0].pick_random()
 	is_being_attacked = [false, true].pick_random()
-	if !is_moving:
+	if can_move:
 		var weight = randf()
 		direction = [Vector2.LEFT, Vector2.RIGHT, Vector2.UP, Vector2.DOWN].pick_random()
 		## Constraining basic movement to the horizontal axis
@@ -63,8 +60,4 @@ func change_sprite_direction() -> void:
 		animated_sprite_2d.flip_h = true
 	else:
 		animated_sprite_2d.flip_h = false
-	
-func hooked() -> void:
-	## if harpoon hits fish
-	is_hooked = true;
 	
--- fish/squid.gd
+++ fish/squid.gd
@@ -1,15 +1,11 @@
 extends Fish 
 
 func move(delta: float) -> void:
-	if is_hooked:
-		## Unable to act, dragged towards the player
-		## if harpoon collides fish hitbox, capture
-		is_hooked = false
-	elif is_being_attacked:
+	if is_being_attacked:
 		speed = 2000
 		change_sprite_direction()
 		velocity += direction * speed * delta
-	elif !is_moving:
+	else:
 		speed = 1000
 		change_sprite_direction()
 		velocity += direction * speed * delta
@@ -23,10 +19,5 @@ func _physics_process(delta: float) -> void:
 func _on_timer_timeout() -> void:
 	timer.wait_time = 2.0
 	is_being_attacked = [false, true].pick_random()
-	if !is_moving:
+	if can_move:
 		direction = [Vector2.LEFT, Vector2.RIGHT].pick_random()
-
-func hooked() -> void:
-	## if harpoon hits fish
-	is_hooked = true;
-	
